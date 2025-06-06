6c3920e0162650aacc7257e31fe85dd3c6b82905
chelobureck <adylbekoveldiyar2@gmail.com>
2025-06-06 07:29:40 +0600

    добавил URL адреса

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ let requestCount = 0;
 
 const server = http.createServer((req, res) => {
     requestCount++;
+
+    switch (req.url) {
+        case '/':
+            res.write('Главная страница');
+            break;
+        case '/about':
+            res.write('О нас');
+            break;
+        case '/contact':
+            res.write('Контакты');
+            break;
+        default:
+            res.write('Страница не найдена');
+    }
+
     res.write('Счтетчик запросов: ' + requestCount);
     res.end();
 });
